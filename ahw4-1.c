5f9fc8996c3d73a97dba0571a7ecbb6bb8bac21e
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 22:00:36 +0900

    Update ahw4-1.c

--- ahw4-1.c
+++ ahw4-1.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main(void)
+{
+    int x;
+    scanf("%d",&x);
+    if (x>0){
+        printf("%d is a positive number",x);
+    }
+    if (x<0){
+        printf("%d is a negative number",x);
+    }
+    if (x==0){
+        printf("You entered 0");
+    }
+    return 0;
+}
