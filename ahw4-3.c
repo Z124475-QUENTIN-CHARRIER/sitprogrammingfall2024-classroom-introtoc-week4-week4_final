11993de0539753b088f962c78e56679f0f8de60a
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 22:34:00 +0900

    Update ahw4-3.c

--- ahw4-3.c
+++ ahw4-3.c
@@ -9,16 +9,16 @@ scanf("%lf", &b);
 printf("c? ");
 scanf("%lf", &c);
 d = b*b - 4*a*c;
-printf("Input is a=%f b=%f c=%f:\n", a, b, c);
+printf("Input is a=%d b=%d c=%d:\n", a, b, c);
 if (d<0){
     printf("The real solution does not exist");
     return 0;
 }
 if (a==0){
-    printf("Output should be %f",-c/d);
+    printf("Output should be %d",-c/d);
 }
 if (d>=0 && a!=0){
-    printf("Output should be %f and %f",(-b+sqrt(d))/2*a,(-b-sqrt(d))/2*a);
+    printf("Output should be %d and %d",(-b+sqrt(d))/2*a,(-b-sqrt(d))/2*a);
 }
 return 0;
 }
