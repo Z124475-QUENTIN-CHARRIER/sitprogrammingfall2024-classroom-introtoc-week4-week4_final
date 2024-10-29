5bd63e1f5cdb3b0c0a4943b9d7a02bf20d65ec81
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 21:49:56 +0900

    Update hw4-5.c

--- hw4-5.c
+++ hw4-5.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main(void)
 {
-	double height, ideal, weight;
-	printf("Height?\n");
-	scanf("%lf", &height);
-	printf("Weight?\n");
-        scanf("%lf", &weight);
-	ideal = height*height*22/10000;
-	printf("Ideal weight = %f\n", ideal);
-	if (weight - ideal > 5)
-		{printf("Let’s reduce sweets\n");}
-        else if (weight - ideal < -5)
-		{printf("Eat some more\n");}
-	else 
-		{ printf("Go for it\n");}
-	return 0;
+    double height, ideal, weight;
+    printf("Height?\n");
+    scanf("%lf", &height);
+    printf("weight?\n");
+    scanf("%lf", &weight);
+    ideal = height * height * 22 / 10000;
+    printf("Ideal weight = %f\n", ideal);
+    if (weight-ideal>5){
+        printf("Let’s reduce sweets\n");
+    }
+    if (ideal-weight>5){
+        printf("Eat some more\n");
+    }
+    if (ideal - weight <= 5 && weight - ideal <= 5){
+        printf("Go for it\n");
+    }
+    return 0;
 }
