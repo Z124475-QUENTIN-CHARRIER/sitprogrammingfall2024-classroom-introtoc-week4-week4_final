a665b76d25b17ad8b9269b624508259041994564
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 15:57:35 +0900

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(void) {
+    int x;
+    scanf("%d",&x);
+    if (x>0){
+        printf("Positive");
+    }
+    if (x<=0){
+        printf("Negative or zero");
+    }
+    return 0;
+}
