85510da0a3f355b987abb262f498a2141547892d
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 20:55:25 +0900

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+   float p,q,E;
+   printf("p? ");
+   scanf("%f", &p);
+   q=1-p;
+   E=-p*log(p)/log(2)-q*log(q)/log(2);
+   printf("%f",E);
+   return 0;
+}
