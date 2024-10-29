a97d1ac4e94d1a02f65fa623d700a5044784128e
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 21:42:41 +0900

    Update hw4-5.c

--- hw4-5.c
+++ hw4-5.c
@@ -2,17 +2,17 @@
 int main(void)
 {
 	double height, ideal, weight;
-	printf(« Height?\n »);
-	scanf(« %lf », &height);
-	printf(« Weight?\n »);
-        scanf(« %lf », &weight);
+	printf("Height?\n");
+	scanf("%lf", &height);
+	printf("Weight?\n");
+        scanf("%lf", &weight);
 	ideal = height*height*22/10000;
-	printf(« Ideal weight = %f\n », ideal);
+	printf("Ideal weight = %f\n", ideal);
 	if (weight - ideal > 5)
-		{printf(« Let’s reduce sweets\n »);}
+		{printf("Let’s reduce sweets\n");}
         else if (weight - ideal < -5)
-		{printf(« Eat some more\n »);}
+		{printf("Eat some more\n");}
 	else 
-		{ printf(« Go for it\n »);}
+		{ printf("Go for it\n");}
 	return 0;
 }
