d95e6be22af86ef4255f6d742810a020425583ce
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 15:33:14 +0900

    Update ahw4-1.c

--- ahw4-1.c
+++ ahw4-1.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int x, y, z;
+    int average;
+    scanf("%d %d %d", &x, &y, &z);
+    average = (x + y + z) / 3;
+    printf("Average is %d\n", average);
+    return 0;
+}
 
