ca13ff56dfd766f8313d0b093edda7b9f393c802
Z124475-QUENTIN-CHARRIER <z124475@shibaura-it.ac.jp>
2024-10-29 22:12:49 +0900

    Update ahw4-2.c

--- ahw4-2.c
+++ ahw4-2.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(void)
+{
+    int x,y,z,max;
+    max=x;
+   scanf("%d %d %d", &x, &y, &z);
+   if (x<y){
+    max=y;
+   }
+   if (z>max){
+    max=z;
+   }
+   printf("Max is %d",max);
+    return 0;
+}
